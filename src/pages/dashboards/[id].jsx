a752334fb355997c03586b99662e05a8565a6a16
unknown <sittikorn1311@gmail.com>
2023-08-23 11:00:54 +0700

    update GrapDashboardsAction to ID result

--- src/pages/dashboards/[id].jsx
+++ src/pages/dashboards/[id].jsx
@@ -9,7 +9,7 @@ import { useGetCampaign_resultQuery } from 'src/store/api'
 // Import Component
 import UserGroupDetailTable from 'src/views/table/UserGroupDetailTable'
 import GrapDashboards from 'src/views/dashboards/GrapDashboards'
-
+import GrapDashboardsAction from 'src/views/dashboards/GrapDashboardsAction'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 const UserGroupDetail = () => {
@@ -41,8 +41,11 @@ const UserGroupDetail = () => {
         {campainsData.status ?? 'Wating...'}
       </Button>
       <Grid container sx={{ my: [0, 4, 1.625] }} spacing={5}>
-        <Grid item xs={12} sx={{ mb: [3, 0] }}>
+        <Grid item xs={12} sm={4} sx={{ mb: [3, 0] }}>
           <GrapDashboards dataGrap={summaryData} />
+        </Grid>
+        <Grid item xs={12} sm={8} sx={{ mb: [3, 0] }}>
+          <GrapDashboardsAction dataGrap={summaryData} />
         </Grid>
       </Grid>
       <Typography variant='h3' sx={{ my: 8 }}>
